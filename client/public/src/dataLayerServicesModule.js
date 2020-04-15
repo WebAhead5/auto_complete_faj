63bffe608d841d38e79767e3eed5b8459e2edfc7
Jake <jakepowis@gmail.com>
2020-04-15 15:40:54 +0300

    further updates to validation

--- client/public/src/dataLayerServicesModule.js
+++ client/public/src/dataLayerServicesModule.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const fs = require('fs')
 let x = 1;
 let serverReadyState = true;
-let callInterval = 2000;
+let callInterval = 200;
 let minStr = 3
 let cache = {}
 
@@ -11,7 +11,7 @@ let cache = {}
 const getData = (str, cb) => {
     try {
         if (str.length >= minStr) {
-            if (newRequest()) {
+            if (newRequest(str)) {
                 if (serverReadyState == true) {
                     timeoutReadyState();
                     //temp load once
@@ -39,68 +39,67 @@ const getData = (str, cb) => {
     catch (error) {
         console.log("Error:", error)
     }
-}
 
-const readWordFile = (str, cb) => {
-    fs.readFile(__dirname + "/textFile.json", (err, data) => {
-        if (err) console.log(err)
-        console.log("FILE DATA:", JSON.parse(data))
-        buildWordArr(str, JSON.parse(data), cb)
-    })
-}
 
-const buildWordArr = (str, data, cb) => {
-    try {
-        const regex = new RegExp(`^${str}`, 'gi');
-        let response = Object.keys(data).filter(word => {
-            return word.match(regex);
-        }).slice(0, 10);
-
-        console.log("Filtered Arr Response:", response)
-        addToCache(str, response)
-        cb(null, response)
+    const readWordFile = (str, cb) => {
+        fs.readFile(__dirname + "/textFile.json", (err, data) => {
+            if (err) console.log(err)
+            buildWordArr(str, JSON.parse(data), cb)
+        })
     }
-    catch (error) {
-        cb(error)
+
+    const buildWordArr = (str, data, cb) => {
+        try {
+            const regex = new RegExp(`^${str}`, 'gi');
+            let response = Object.keys(data).filter(word => {
+                return word.match(regex);
+            }).slice(0, 10);
+
+            addToCache(str, response)
+            cb(null, response)
+        }
+        catch (error) {
+            cb(error)
+        }
     }
-}
 
-const apiCall = (url) => {
-    axios.get(url)
-        .then(response => { return response })
-        .catch(error => { console.log("The errrrrrrror", error) })
-}
-
-const timeoutReadyState = () => {
-    serverReadyState = false
-    setTimeout(() => {
-        serverReadyState = true
-        console.log("TIMER SETS READY STATE TO:", serverReadyState)
-    }, callInterval)
-}
-
-const addToCache = (str, response) => {
-    //if (error) cb(error)
-    let arr = []
-    response.forEach(x => arr.push(x))
-    cache[str] = arr;
-    console.log("cache add: ", cache, "end of cache")
-}
-
-const pullFromCache = (str, cb) => {
-    //if (error) cb(error)
-    console.log("cache pull: ", cache, "end of cache")
-    cb(null, cache[str])
-}
-
-const newRequest = (str) => {
-    if (Object.keys(cache).some(x => x == str)) {
-        console.log("This has been enetered before")
-        return false
+    const apiCall = (url) => {
+        axios.get(url)
+            .then(response => { return response })
+            .catch(error => { console.log("The errrrrrrror", error) })
     }
-    return true
-}
 
+    const timeoutReadyState = () => {
+        serverReadyState = false
+        setTimeout(() => {
+            serverReadyState = true
+            console.log("TIMER SETS READY STATE TO:", serverReadyState)
+        }, callInterval)
+    }
+
+    const addToCache = (str, response) => {
+        //if (error) cb(error)
+        let arr = []
+        response.forEach(x => arr.push(x))
+        cache[str] = arr;
+        console.log("cache add: ", cache, "end of cache")
+    }
+
+    const pullFromCache = (str, cb) => {
+        //if (error) cb(error)
+        console.log("cache pull: ", cache, "end of cache")
+        cb(null, cache[str])
+    }
+
+    const newRequest = (str) => {
+        if (Object.keys(cache).some(x => x == str)) {
+            console.log("This has been enetered before")
+            return false
+        }
+        return true
+    }
+
+}
 
 module.exports = {
     getData: getData
