01135e07d4705539382de2c344a663016d66d575
Jake <jakepowis@gmail.com>
2020-04-16 12:42:34 +0300

    adding comments to code

--- server/src/dataLayerServicesModule.js
+++ server/src/dataLayerServicesModule.js
@@ -7,14 +7,14 @@ let minStr = 2
 let cache = {}
 
 
-
-//GET DATA FUNCTION ####################################################
+//GET DATA MODULE ####################################################
 
 const getData = (str, cb) => {
 
 
-    //Schedule API call every midnight
+    //API FUNCTIONS ###################################################
 
+    //Schedule API call every midnight to update JSON file
     schedule.scheduleJob('00***', () => {
 
         const url = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&rows=10000&facet=country&refine.country=il"
@@ -23,7 +23,22 @@ const getData = (str, cb) => {
     })
 
 
-    //PURE FUNCTIONS ###################################################
+    const apiCall = (url) => {
+        axios.get(url)
+            .then(response => {
+                let dataArr = []
+                response.data.records.forEach(item => dataArr.push(item.fields.city))
+                let arrJSON = JSON.stringify(dataArr)
+
+                fs.writeFile(__dirname + '/../cityList.json', arrJSON, (err) => {
+                    if (err) console.log(err)
+                    console.log("JSON FILE UPDATED FROM API")
+                })
+                    .catch(error => { console.log("API LOAD ERROR:", error) })
+            })
+    }
+
+    //PRACTICAL FUNCTIONS #############################################
 
 
     const readWordFile = (str, cb) => {
@@ -48,27 +63,25 @@ const getData = (str, cb) => {
         }
     }
 
-    const apiCall = (url) => {
-        axios.get(url)
-            .then(response => {
-                let dataArr = []
-                response.data.records.forEach(item => dataArr.push(item.fields.city))
-                let arrJSON = JSON.stringify(dataArr)
-
-                fs.writeFile(__dirname + '/../cityList.json', arrJSON, (err) => {
-                    if (err) console.log(err)
-                    console.log("JSON FILE UPDATED FROM API")
-                })
-                    .catch(error => { console.log("API LOAD ERROR:", error) })
-            })
-    }
-
+    //To prevent too frequent calls for data, time set above in CallInterval
     const timeoutReadyState = () => {
         serverReadyState = false
         setTimeout(() => {
             serverReadyState = true
             console.log("TIMER SETS READY STATE TO:", serverReadyState)
         }, callInterval)
+    }
+
+
+    // CACHE FUNCTIONS ################################################
+
+    //Check if request is in cache
+    const newRequest = (str) => {
+        if (Object.keys(cache).some(x => x == str)) {
+            console.log("This has been enetered before")
+            return false
+        }
+        return true
     }
 
     const addToCache = (str, response) => {
@@ -85,13 +98,7 @@ const getData = (str, cb) => {
         cb(null, cache[str])
     }
 
-    const newRequest = (str) => {
-        if (Object.keys(cache).some(x => x == str)) {
-            console.log("This has been enetered before")
-            return false
-        }
-        return true
-    }
+
 
     // DATA HANDLER ################################################
 
