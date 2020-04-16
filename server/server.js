f77f280bd8a27d3893915fede09a65dfa0b94c06
fridabu <f0504070002@gmail.com>
2020-04-16 12:55:37 +0300

    update commit in server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
+/*
+//This file was imprtant to seperate it from app server 
+//because a problem we have in the tests:
+//we say that superTest module runs the server (we don't want that)
+//So our solution was to seperate the listen to diffrent file
+//
+*/
+
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
